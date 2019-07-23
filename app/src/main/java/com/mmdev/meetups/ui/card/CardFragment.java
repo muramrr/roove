419df93bf0458a8eacbcffcfaff58a9404caf617
muram4ik@gmail.com <muramrr>
2019-07-23 21:10:55 +0300

    alpha version likes skips getters

--- app/src/main/java/com/mmdev/meetups/ui/card/CardFragment.java
+++ app/src/main/java/com/mmdev/meetups/ui/card/CardFragment.java
@@ -10,9 +10,7 @@ import android.widget.Toast;
 
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QuerySnapshot;
 import com.mmdev.meetups.R;
 import com.mmdev.meetups.models.ProfileModel;
 import com.mmdev.meetups.ui.main.MainActivity;
@@ -22,13 +20,15 @@ import com.yuyakaido.android.cardstackview.CardStackListener;
 import com.yuyakaido.android.cardstackview.CardStackView;
 import com.yuyakaido.android.cardstackview.Direction;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
+
+
+/* Todo: move data getters to another class */
 
 public class CardFragment extends Fragment {
 	
@@ -50,14 +50,15 @@ public class CardFragment extends Fragment {
 	private CardStackView cardStackView;
 	private CardStackAdapter mCardStackAdapter;
 	private CardStackLayoutManager mCardStackLayoutManager;
-	
-	
+	private CardsViewModel mCardsViewModel;
+	private List<ProfileModel> mPotentialUsersList;
 	private ProfileModel mProfileModel, mSwipeUser;
 	private String mPreferedGender;
 	private ProgressBar pbStatus;
 	private int limit, total_users;
+	private boolean mProgressShowing;
 	
-	private static List<ProfileModel> mUsersCards = new ArrayList<>();
+	
 
 	@Nullable
 	@Override
@@ -70,23 +71,50 @@ public class CardFragment extends Fragment {
 		if (getActivity() != null) mMainActivity = (MainActivity) getActivity();
 		cardStackView = view.findViewById(R.id.card_stack_view);
 		pbStatus = view.findViewById(R.id.card_prBar);
-		mCardStackAdapter = new CardStackAdapter(mUsersCards);
 		ProfileViewModel mProfileViewModel = ViewModelProviders.of(mMainActivity).get(ProfileViewModel.class);
 		mProfileModel = mProfileViewModel.getProfileModel(mMainActivity).getValue();
+		
+		mCardsViewModel = ViewModelProviders.of(mMainActivity).get(CardsViewModel.class);
+		
 		mFirestore = FirebaseFirestore.getInstance();
 		limit = 1;
+		mProgressShowing = false;
 		if (mProfileModel != null) {
+			showLoadingBar();
+			
+			mPreferedGender = mProfileModel.getPreferedGender();
 			mUsersCollection = mFirestore.collection(USERS_COLLECTION_REFERENCE);
 			mProfileDocument = mUsersCollection.document(mProfileModel.getUserID());
-			showLoadingBar();
-			mUsersCards.clear();
-			getPotentialUsers();
+			
+			mCardsViewModel
+					.getUsers(mProfileModel.getPreferedGender(), mProfileModel.getUserID())
+					.observe(mMainActivity, profileModelList -> {
+						mPotentialUsersList = profileModelList;
+						if(mCardStackAdapter == null)
+							mCardStackAdapter = new CardStackAdapter(mPotentialUsersList);
+						cardStackView.setAdapter(mCardStackAdapter);
+						if(mCardStackAdapter.getItemCount() != 0) {
+							hideLoadingBar();
+							mCardStackAdapter.notifyDataSetChanged();
+						}
+					});
+			
+			
 		}
 		
+		
+		
 		mCardStackLayoutManager = new CardStackLayoutManager(mMainActivity, new CardStackListener() {
+			
 			@Override
 			public void onCardAppeared (View view, int position) {
-				mSwipeUser = mCardStackAdapter.getSwipeProfile(position);
+				mSwipeUser = mCardStackAdapter.getSwipeProfile(0);
+				Toast.makeText(
+						mMainActivity,
+						"cards= "+position+" adapter = "+mCardStackAdapter.getItemCount(),
+						Toast.LENGTH_SHORT
+				).show();
+				
 			}
 		
 			@Override
@@ -96,14 +124,14 @@ public class CardFragment extends Fragment {
 
 			@Override
 			public void onCardSwiped (Direction direction) {
-				if(direction == Direction.Right) {
+				if(direction == Direction.Right)
 					mProfileDocument.collection(USER_LIKES_COLLECTION_REFERENCE)
 							.document(mSwipeUser.getUserID())
 							.set(mSwipeUser);
-				}
 				else mProfileDocument.collection(USER_SKIPS_COLLECTION_REFERENCE)
 						.document(mSwipeUser.getUserID())
 						.set(mSwipeUser);
+				//mPotentialUsersList.remove(mSwipeUser);
 			}
 
 			@Override
@@ -114,14 +142,16 @@ public class CardFragment extends Fragment {
 			
 			@Override
 			public void onCardDisappeared (View view, int position) {
-				if (position == total_users-1) {
+				
+				if (position == mCardStackAdapter.getItemCount()-1) {
+					
 					showLoadingBar();
-					getPotentialUsers();
 				}
 			}
 		});
+		
 		cardStackView.setLayoutManager(mCardStackLayoutManager);
-		cardStackView.setAdapter(mCardStackAdapter);
+		
 	}
 
 	@Override
@@ -129,35 +159,24 @@ public class CardFragment extends Fragment {
 		super.onActivityCreated(savedInstanceState);
 	}
 	
-	private void getPotentialUsers(){
-		mPreferedGender = mProfileModel.getPreferedGender();
-		mUsersCollection.whereEqualTo(USERS_FILTER, mPreferedGender)
-				//.limit(limit)
-				.get()
-				.addOnCompleteListener(task -> {
-					if (task.isSuccessful() && task.getResult()!=null) {
-						QuerySnapshot result = task.getResult();
-						total_users = result.getDocuments().size();
-						for (DocumentSnapshot doc :result)
-							if (!doc.getId().equals(mProfileModel.getUserID()))
-								mUsersCards.add(doc.toObject(ProfileModel.class));
-						
-						mCardStackAdapter.notifyDataSetChanged();
-						mCardStackLayoutManager.setTopPosition(0);
-						hideLoadingBar();
-					}
-				})
-				.addOnFailureListener(e -> Toast.makeText(mMainActivity, "Cannot retrieve information", Toast.LENGTH_SHORT).show());
-	}
-	
 	private void showLoadingBar(){
-		cardStackView.setVisibility(View.GONE);
-		pbStatus.setVisibility(View.VISIBLE);
+		if (!mProgressShowing) {
+			cardStackView.setVisibility(View.GONE);
+			pbStatus.setVisibility(View.VISIBLE);
+			mProgressShowing = true;
+		}
 	}
 	
-	private void hideLoadingBar(){
-		pbStatus.setVisibility(View.GONE);
-		cardStackView.setVisibility(View.VISIBLE);
+	private void hideLoadingBar() {
+		if (mProgressShowing){
+			pbStatus.setVisibility(View.GONE);
+			cardStackView.setVisibility(View.VISIBLE);
+			mProgressShowing = false;
+		}
 	}
-
+	
+	private boolean checkMatch() {
+		return true;
+	}
+	
 }
