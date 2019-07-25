95c9850bec5b1a7261ab6b71602c98cb75a8cd91
muram4ik@gmail.com <muramrr>
2019-07-25 21:55:29 +0300

    alpha match check fixed some bugs likes skips getters

--- app/src/main/java/com/mmdev/meetups/ui/card/CardsViewModel.java
+++ app/src/main/java/com/mmdev/meetups/ui/card/CardsViewModel.java
@@ -18,7 +18,11 @@ import androidx.lifecycle.ViewModel;
 /* Created by A on 20.07.2019.*/
 
 /**
- * This is the documentation block about the class
+ * get users from firebase firestore
+ * getAllUsers -> getSkipedUsers -> getLikedUsers -> getMatchedUsers -> mergeLikedSkipedMatched ->
+ * -> create new list from getAllUsersCards list that does not contains mergedLikedSkipedMatched items
+ * -> postValue into LiveData variable ... else return null and show loading bar in Fragment class
+ * TODO: MAKE ASYNC DATA RETRIEVE AND FETCH DYNAMICALLY
  */
 
 public class CardsViewModel extends ViewModel {
@@ -29,12 +33,15 @@ public class CardsViewModel extends ViewModel {
 	private static final String USER_SKIPS_COLLECTION_REFERENCE = "skips";
 	private static final String USER_MATCHES_COLLECTION_REFERENCE = "matches";
 	
+	private String mCurrentUserId;
+	
 	private CollectionReference mUsersCollection;
 	private DocumentReference mProfileDocument;
 	
 	private List<ProfileModel> mAllUsersCards = new ArrayList<>();
-	private List<String> mLikedUsersCardsIds = new ArrayList<>();
 	private List<String> mSkipedUsersCardsIds = new ArrayList<>();
+	private List<String> mLikedUsersCardsIds = new ArrayList<>();
+	private List<String> mMatchedUsersCardsIds = new ArrayList<>();
 	
 	
 	private MutableLiveData<List<ProfileModel>> potentialUsersCards;
@@ -45,15 +52,12 @@ public class CardsViewModel extends ViewModel {
 		FirebaseFirestore mFirestore = FirebaseFirestore.getInstance();
 		mUsersCollection = mFirestore.collection(USERS_COLLECTION_REFERENCE);
 		mProfileDocument = mUsersCollection.document(userId);
+		mCurrentUserId = userId;
 		if (potentialUsersCards == null) {
 			potentialUsersCards = new MutableLiveData<>();
 			loadUsers(preferedGender, userId);
 		}
 		
-		return potentialUsersCards;
-	}
-	
-	public LiveData<List<ProfileModel>> getUsers() {
 		return potentialUsersCards;
 	}
 	
@@ -90,7 +94,7 @@ public class CardsViewModel extends ViewModel {
 				.addOnCompleteListener(task -> {
 					if(task.getResult() != null) {
 						for (DocumentSnapshot doc :task.getResult().getDocuments())
-							mSkipedUsersCardsIds.add(doc.toObject(ProfileModel.class).getUserID());
+							mSkipedUsersCardsIds.add(doc.getId());
 						getLikedUsersCards();
 						Log.wtf("logs","skips on complete, size = "+ mSkipedUsersCardsIds.size());
 					}
@@ -110,21 +114,42 @@ public class CardsViewModel extends ViewModel {
 				.addOnCompleteListener(task -> {
 					if(task.getResult() != null) {
 						for (DocumentSnapshot doc :task.getResult().getDocuments())
-							mLikedUsersCardsIds.add(doc.toObject(ProfileModel.class).getUserID());
+							mLikedUsersCardsIds.add(doc.getId());
 						
 						Log.wtf("logs","likes on complete, size = "+ mLikedUsersCardsIds.size());
-						merge();
+						getMatchedUsersCards();
 					}
 				})
 				.addOnFailureListener(e -> Log.wtf("logs","liked fail"));
 		
 	}
 	
-	private void merge(){
+	/*
+	 * GET MATCHED
+	 */
+	private void getMatchedUsersCards(){
+		mProfileDocument.collection(USER_MATCHES_COLLECTION_REFERENCE)
+				.get()
+				.addOnCompleteListener(task -> {
+					if(task.getResult() != null) {
+						for (DocumentSnapshot doc :task.getResult().getDocuments())
+							mMatchedUsersCardsIds.add(doc.getId());
+						
+						Log.wtf("logs","matches on complete, size = "+ mMatchedUsersCardsIds.size());
+						mergeLikedSkipedMatched();
+					}
+				})
+				.addOnFailureListener(e -> Log.wtf("logs","matches fail"));
+		
+	}
+	
+	//merge liked, skiped, matched lists
+	private void mergeLikedSkipedMatched (){
 		List<String> mergedLikesSkipsCardsIds = new ArrayList<>(mLikedUsersCardsIds);
 		mergedLikesSkipsCardsIds.addAll(mSkipedUsersCardsIds);
-		List<ProfileModel> returningModelsIds = new ArrayList<>();
+		mergedLikesSkipsCardsIds.addAll(mMatchedUsersCardsIds);
 		if (mergedLikesSkipsCardsIds.size() != 0) {
+			List<ProfileModel> returningModelsIds = new ArrayList<>();
 			for (ProfileModel profileModel: mAllUsersCards)
 				if (!mergedLikesSkipsCardsIds.contains(profileModel.getUserID())) returningModelsIds.add(profileModel);
 			Log.wtf("logs","potential users available = " + returningModelsIds.size());
@@ -132,9 +157,31 @@ public class CardsViewModel extends ViewModel {
 		}
 		else {
 			potentialUsersCards.postValue(mAllUsersCards);
-			Log.wtf("logs","likes and skips = 0 ");
+			Log.wtf("logs","likes + skips + matches = 0 ");
 		}
 		
+	}
+	
+	/*
+	* check if users liked each other
+	*/
+	public boolean checkMatch(String uId) {
+		
+		mUsersCollection.document(uId).collection(USER_LIKES_COLLECTION_REFERENCE)
+				.document(mCurrentUserId)
+				.get()
+				.addOnSuccessListener(documentSnapshot -> {
+			if (documentSnapshot.exists()) {
+				Log.wtf("logs", "like from that side exists");
+				//return true;
+			}
+			
+		})
+		.addOnFailureListener(e -> {
+			Log.wtf("logs","checkMatch fail");
+		});
+		
+		return false;
 	}
 	
 }
