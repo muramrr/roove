c07d9b67c4fb19e20712fecf52f582c206a847ea
muram4ik@gmail.com <muramrr>
2019-08-18 17:15:36 +0300

    chat refract

--- app/src/main/java/com/mmdev/meetapp/models/FileModel.kt
+++ app/src/main/java/com/mmdev/meetapp/models/FileModel.kt
@@ -2,7 +2,7 @@ package com.mmdev.meetapp.models
 
 /* Created by A on 06.06.2019.*/
 
-data class FileModel(var fileType: String? = "",
-                     var fileUrl: String? = "",
-                     var fileName: String? = "",
+data class FileModel(var fileType: String = "",
+                     var fileUrl: String = "",
+                     var fileName: String = "",
                      var fileSize: String? = "")
