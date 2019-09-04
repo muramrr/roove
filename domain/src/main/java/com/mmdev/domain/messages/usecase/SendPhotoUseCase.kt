325c91a3862cfdfbec28780fea0028805bbefcf7
muram4ik@gmail.com <muramrr>
2019-09-04 19:51:58 +0300

    fixed chat info photo attach WIP

--- domain/src/main/java/com/mmdev/domain/messages/usecase/SendPhotoUseCase.kt
+++ domain/src/main/java/com/mmdev/domain/messages/usecase/SendPhotoUseCase.kt
@@ -1,15 +1,12 @@
 package com.mmdev.domain.messages.usecase
 
-import com.mmdev.domain.core.CompletableMultipleParamUseCase
-import com.mmdev.domain.messages.model.Message
 import com.mmdev.domain.messages.repository.ChatRepository
-import java.io.File
 
 /* Created by A on 26.08.2019.*/
 
-class SendPhotoUseCase (private val repository: ChatRepository) :
-		CompletableMultipleParamUseCase<Message,File> {
+class SendPhotoUseCase (private val repository: ChatRepository){
 
-	override fun execute(t1: Message, t2: File) = repository.sendPhoto(t1, t2)
+	fun execute(t: String) = repository.sendPhoto(t)
+
 }
 
