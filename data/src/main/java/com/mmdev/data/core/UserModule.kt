b7ff3595d605aa76b12364bade4fdff94dc3b140
Andrii Kovalchuk <muram4ik@gmail.com>
2019-09-29 15:01:17 +0300

    gradle update

---
+++ data/src/main/java/com/mmdev/data/core/UserModule.kt
@@ -0,0 +1,31 @@
+package com.mmdev.data.core
+
+import android.content.Context
+import com.mmdev.data.user.UserRepository
+import com.mmdev.domain.core.model.User
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+
+/* Created by A on 16.09.2019.*/
+
+/**
+ * This is the documentation block about the class
+ */
+
+@Module
+class UserModule (private val context: Context) {
+
+
+	@Provides
+	@Singleton
+	fun provideContext(): Context { return context }
+
+
+	/* get user info from sharedPrefs */
+	@Provides
+	@Singleton
+	fun provideSavedUser(repository: UserRepository): User = repository.getSavedUser()
+
+}
