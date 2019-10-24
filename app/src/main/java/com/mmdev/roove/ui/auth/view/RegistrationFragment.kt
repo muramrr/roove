46c1c8803fb851fc8e977160b2f0b24025ee40d2
Andrii Kovalchuk <muram4ik@gmail.com>
2019-10-24 17:59:47 +0300

    fully redesigned app name + logo + colors + all screens

---
+++ app/src/main/java/com/mmdev/roove/ui/auth/view/RegistrationFragment.kt
@@ -0,0 +1,60 @@
+package com.mmdev.roove.ui.auth.view
+
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import co.ceryle.segmentedbutton.SegmentedButtonGroup
+import com.mmdev.roove.R
+import com.mmdev.roove.ui.custom.ProgressButton
+
+/* Created by A on 23.10.2019.*/
+
+/**
+ * This is the documentation block about the class
+ */
+
+class RegistrationFragment: Fragment(R.layout.activity_auth_fragment_reg){
+
+	private lateinit var mAuthActivity: AuthActivity
+	lateinit var progressButton: ProgressButton
+
+	override fun onAttach(context: Context) {
+		super.onAttach(context)
+		activity?.let { mAuthActivity = it as AuthActivity }
+		mAuthActivity.hideFacebookButton()
+	}
+
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+
+		var gender = "male"
+		var preferedGender = "male"
+		val sbgGender = view.findViewById(R.id.dialog_registr_sbg_gender) as SegmentedButtonGroup
+		val sbgPrefgender = view.findViewById(R.id.dialog_registr_sbg_preferedgender) as SegmentedButtonGroup
+
+		sbgGender.setOnClickedButtonListener {
+			position -> gender = if (position == 0) "male" else "female"
+		}
+
+		sbgPrefgender.setOnClickedButtonListener { position ->
+			when (position) {
+				0 -> preferedGender = "male"
+				1 -> preferedGender = "female"
+				2 -> preferedGender = "both"
+			}
+		}
+		progressButton = view.findViewById(R.id.diag_reg_btn_done) as ProgressButton
+		progressButton.setOnClickListener {
+			progressButton.startAnim()
+			mAuthActivity.userModel.gender = gender
+			mAuthActivity.userModel.preferedGender = preferedGender
+			mAuthActivity.authCallback(progressButton)
+		}
+	}
+
+	override fun onStop() {
+		super.onStop()
+		mAuthActivity.showFacebookButton()
+	}
+}
---
+++ app/src/main/java/com/mmdev/roove/ui/messages/MessagesFragment.java
@@ -0,0 +1,35 @@
+package com.mmdev.roove.ui.messages;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.mmdev.roove.R;
+import com.mmdev.roove.ui.main.view.MainActivity;
+
+
+public class MessagesFragment extends Fragment {
+	
+	
+	private MainActivity mMainActivity;
+	
+	@Nullable
+	@Override
+	public View onCreateView (@NonNull LayoutInflater inflater,
+							  @Nullable ViewGroup container,
+							  @Nullable Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.fragment_messages, container, false);
+	}
+	
+	@Override
+	public void onViewCreated (@NonNull View view, @Nullable Bundle savedInstanceState) {
+		if (getActivity() != null) mMainActivity = (MainActivity) getActivity();
+		
+		
+	}
+}
