39e066a7b253f6e1d3c583138570158a78a93c67
Andrii Kovalchuk <andrii.muramrr@gmail.com>
2019-12-06 21:28:39 +0200

    beta navigation component

--- app/src/main/java/com/mmdev/roove/ui/drawerflow/viewmodel/remote/RemoteUserRepoViewModel.kt
+++ app/src/main/java/com/mmdev/roove/ui/drawerflow/viewmodel/remote/RemoteUserRepoViewModel.kt
@@ -1,14 +1,14 @@
 /*
  * Created by Andrii Kovalchuk
  * Copyright (c) 2019. All rights reserved.
- * Last modified 05.12.19 15:34
+ * Last modified 06.12.19 17:52
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-package com.mmdev.roove.ui.main.viewmodel.remote
+package com.mmdev.roove.ui.drawerflow.viewmodel.remote
 
 import androidx.lifecycle.ViewModel
 import com.mmdev.business.user.usecase.remote.CreateUserUseCase
