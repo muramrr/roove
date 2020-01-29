e4ec97a7793c2ab36cea9a3f1ff0a128289079e0
Andrii Kovalchuk <andrii.muramrr@gmail.com>
2020-01-29 18:48:48 +0200

    fixed bug to adding same registration tokens each time app starts removed some unnecessary calls to rewrite same objects in prefs fixed shuffle order while getting set of strings more convenient logs to understand where exactly function was called notifications seems to works more stable

--- business/src/main/java/com/mmdev/business/user/BaseUserInfo.kt
+++ business/src/main/java/com/mmdev/business/user/BaseUserInfo.kt
@@ -1,7 +1,7 @@
 /*
  * Created by Andrii Kovalchuk
  * Copyright (c) 2020. All rights reserved.
- * Last modified 29.01.20 16:42
+ * Last modified 29.01.20 18:44
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
