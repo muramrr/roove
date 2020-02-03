f6237895bdf5776c41126879a2bcc855dd9f0658
Andrii Kovalchuk <muram4ik@gmail.com>
2020-02-03 19:38:45 +0200

    removed unnecessary use case

--- business/src/main/java/com/mmdev/business/chat/repository/ChatRepository.kt
+++ business/src/main/java/com/mmdev/business/chat/repository/ChatRepository.kt
@@ -1,7 +1,7 @@
 /*
  * Created by Andrii Kovalchuk
  * Copyright (c) 2020. All rights reserved.
- * Last modified 30.01.20 20:49
+ * Last modified 03.02.20 19:10
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -18,8 +18,6 @@ import io.reactivex.Observable
 import io.reactivex.Single
 
 interface ChatRepository {
-
-	fun getConversationById(conversationId: String): Single<ConversationItem>
 
 	fun getConversationWithPartner(partnerId: String): Single<ConversationItem>
 
