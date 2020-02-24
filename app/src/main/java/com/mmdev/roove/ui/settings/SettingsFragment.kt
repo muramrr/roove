d8bfd058f254c6f9f4bd221fd3018193c327745c
Andrii Kovalchuk <muram4ik@gmail.com>
2020-02-24 16:43:28 +0200

    template for uploading new photos to profile (same logic as used in ChatFragment)

--- app/src/main/java/com/mmdev/roove/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/mmdev/roove/ui/settings/SettingsFragment.kt
@@ -1,7 +1,7 @@
 /*
  * Created by Andrii Kovalchuk
  * Copyright (c) 2020. All rights reserved.
- * Last modified 24.02.20 16:25
+ * Last modified 24.02.20 16:42
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -10,11 +10,18 @@
 
 package com.mmdev.roove.ui.settings
 
+import android.app.Activity
+import android.content.Intent
 import android.graphics.Rect
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.text.format.DateFormat
 import android.view.Gravity
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.core.content.FileProvider
 import androidx.core.os.bundleOf
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -25,14 +32,16 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.SnapHelper
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.mmdev.business.user.UserItem
+import com.mmdev.roove.BuildConfig
 import com.mmdev.roove.R
 import com.mmdev.roove.ui.auth.AuthViewModel
-import com.mmdev.roove.ui.core.BaseFragment
-import com.mmdev.roove.ui.core.SharedViewModel
+import com.mmdev.roove.ui.core.*
 import com.mmdev.roove.ui.custom.HorizontalCarouselLayoutManager
 import com.mmdev.roove.ui.profile.view.PlacesToGoAdapter
 import com.mmdev.roove.utils.observeOnce
 import kotlinx.android.synthetic.main.fragment_settings.*
+import java.io.File
+import java.util.*
 import kotlin.math.round
 
 
@@ -49,6 +58,14 @@ class SettingsFragment: BaseFragment(R.layout.fragment_settings) {
 	private lateinit var authViewModel: AuthViewModel
 
 	private lateinit var userItem: UserItem
+
+	// File
+	private lateinit var mFilePathImageCamera: File
+
+	companion object {
+		private const val IMAGE_GALLERY_REQUEST = 1
+		private const val IMAGE_CAMERA_REQUEST = 2
+	}
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
@@ -113,12 +130,12 @@ class SettingsFragment: BaseFragment(R.layout.fragment_settings) {
 			val builder = AlertDialog.Builder(it.context)
 				.setItems(arrayOf("Camera", "Gallery")) {
 					_, itemIndex ->
-					if (itemIndex == 0) { }
-					else { }
+					if (itemIndex == 0) { photoCameraClick() }
+					else { photoGalleryClick() }
 				}
 			val alertDialog = builder.create()
 			val params = alertDialog.window?.attributes
-			params?.gravity = Gravity.BOTTOM
+			params?.gravity = Gravity.CENTER
 			alertDialog.show()
 		}
 	}
@@ -150,5 +167,97 @@ class SettingsFragment: BaseFragment(R.layout.fragment_settings) {
 			.show()
 
 	}
+
+	/*
+	 * Checks if the app has permissions to OPEN CAMERA and take photos
+	 * If the app does not has permission then the user will be prompted to grant permissions
+	 * else open camera intent
+	 */
+	private fun photoCameraClick() {
+		handlePermission(AppPermission.CAMERA,
+		                 onGranted = { startCameraIntent() },
+		                 onDenied = { requestPermissionsList(it) },
+		                 onExplanationNeeded = { it.explanationMessageId }
+		)
+	}
+
+	//take photo directly by camera
+	private fun startCameraIntent() {
+		val namePhoto = DateFormat.format("yyyy-MM-dd_hhmmss", Date()).toString()
+		mFilePathImageCamera = File(context?.getExternalFilesDir(Environment.DIRECTORY_PICTURES),
+		                            userItem.baseUserInfo.name + namePhoto + "camera.jpg")
+		val photoURI = FileProvider.getUriForFile(context!!,
+		                                          BuildConfig.APPLICATION_ID + ".provider",
+		                                          mFilePathImageCamera)
+		val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE).apply {
+			putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+		}
+		startActivityForResult(intent, IMAGE_CAMERA_REQUEST)
+	}
+
+	/*
+	 * Checks if the app has permissions to READ user files
+	 * If the app does not has permission then the user will be prompted to grant permissions
+	 * else open gallery to choose photo
+	 */
+	private fun photoGalleryClick() {
+		handlePermission(AppPermission.GALLERY,
+		                 onGranted = { startGalleryIntent() },
+		                 onDenied = { requestPermissionsList(it) },
+		                 onExplanationNeeded = { it.explanationMessageId }
+		)
+	}
+
+	//open gallery chooser
+	private fun startGalleryIntent() {
+		val intent = Intent().apply {
+			action = Intent.ACTION_GET_CONTENT
+			type = "image/*"
+		}
+		startActivityForResult(Intent.createChooser(intent, "Select image"),
+		                       IMAGE_GALLERY_REQUEST)
+	}
+
+	// start after permissions was granted
+	// If request is cancelled, the result arrays are empty.
+	override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+		super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+		onRequestPermissionsResultReceived(requestCode, grantResults,
+		                                   onPermissionGranted = {
+			                                   when (it){
+				                                   AppPermission.CAMERA -> startCameraIntent()
+				                                   AppPermission.GALLERY -> startGalleryIntent()
+			                                   }
+		                                   },
+		                                   onPermissionDenied = { it.deniedMessageId })
+	}
+
+
+	override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+		super.onActivityResult(requestCode, resultCode, data)
+		// send photo from gallery
+		if (requestCode == IMAGE_GALLERY_REQUEST) {
+			if (resultCode == Activity.RESULT_OK) {
+
+				val selectedUri = data?.data
+				//send photo selectedUri.toString()
+			}
+		}
+		// send photo taken by camera
+		if (requestCode == IMAGE_CAMERA_REQUEST) {
+			if (resultCode == Activity.RESULT_OK) {
+
+				if (mFilePathImageCamera.exists()) {
+					//sendPhoto(Uri.fromFile(mFilePathImageCamera).toString(), userItemModel.baseUserInfo, partnerId)
+				}
+				else Toast.makeText(context,
+				                    "filePathImageCamera is null or filePathImageCamera isn't exists",
+				                    Toast.LENGTH_LONG)
+					.show()
+			}
+		}
+	}
+
+
 
 }
