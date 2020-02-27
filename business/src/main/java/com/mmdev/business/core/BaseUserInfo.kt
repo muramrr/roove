9ae5ae392e46bda22b9f9387ea4dfdd0fe423481
Andrii Kovalchuk <muram4ik@gmail.com>
2020-02-27 16:04:50 +0200

    passed away from deleting subcollections just mark it with conversation deleted flag and thats it

--- business/src/main/java/com/mmdev/business/core/BaseUserInfo.kt
+++ business/src/main/java/com/mmdev/business/core/BaseUserInfo.kt
@@ -1,7 +1,7 @@
 /*
  * Created by Andrii Kovalchuk
  * Copyright (c) 2020. All rights reserved.
- * Last modified 27.02.20 15:53
+ * Last modified 27.02.20 15:57
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
