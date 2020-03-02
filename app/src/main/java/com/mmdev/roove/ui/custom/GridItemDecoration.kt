e29b1aa690213af8e677316ac604369781ffee4d
Andrii Kovalchuk <muram4ik@gmail.com>
2020-03-02 18:25:54 +0200

    extracted some error hints to resources added new User field "cityToDisplay" used to display directly where it needed and avoid mapping from same list each time added dimens for different screens inside edit settings photo items added custom grid divider replaced old delete icon fixed bug not displaying values from dropdown list

---
+++ app/src/main/java/com/mmdev/roove/ui/custom/GridItemDecoration.kt
@@ -0,0 +1,47 @@
+/*
+ * Created by Andrii Kovalchuk
+ * Copyright (c) 2020. All rights reserved.
+ * Last modified 02.03.20 16:38
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package com.mmdev.roove.ui.custom
+
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ItemDecoration
+
+
+/**
+ * This is the documentation block about the class
+ */
+
+class GridItemDecoration: ItemDecoration() {
+
+	override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
+
+		val position = parent.getChildAdapterPosition(view)
+		val spanIndex = (view.layoutParams as GridLayoutManager.LayoutParams).spanIndex
+
+		// Add top margin only for the first item to avoid double space between items
+		if (position == 0 || position == 1) {
+			outRect.top = 30
+		}
+
+		if (spanIndex == 0) {
+			outRect.left = 30
+			outRect.right = 15
+		}
+		else { //if you just have 2 span . Or you can use (staggeredGridLayoutManager.getSpanCount()-1) as last span
+			outRect.left = 15
+			outRect.right = 30
+		}
+		outRect.bottom = 30
+	}
+
+}
