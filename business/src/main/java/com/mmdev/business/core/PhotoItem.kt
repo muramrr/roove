6c67360ab7c6a37517b9c2ac7c486a209cb8047d
Andrii Kovalchuk <muram4ik@gmail.com>
2020-03-02 20:10:27 +0200

    check is photo from facebook default

--- business/src/main/java/com/mmdev/business/core/PhotoItem.kt
+++ business/src/main/java/com/mmdev/business/core/PhotoItem.kt
@@ -1,7 +1,7 @@
 /*
  * Created by Andrii Kovalchuk
  * Copyright (c) 2020. All rights reserved.
- * Last modified 02.03.20 19:36
+ * Last modified 02.03.20 20:05
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
