57960f6a5f942bb2b59b43175aaf5f4d7f9dc693
Andrii Kovalchuk <muram4ik@gmail.com>
2020-03-07 19:23:26 +0200

    updated time

--- app/src/main/java/com/mmdev/roove/ui/SharedViewModel.kt
+++ app/src/main/java/com/mmdev/roove/ui/SharedViewModel.kt
@@ -1,7 +1,7 @@
 /*
  * Created by Andrii Kovalchuk
  * Copyright (c) 2020. All rights reserved.
- * Last modified 07.03.20 13:56
+ * Last modified 07.03.20 19:17
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
--- app/src/main/java/com/mmdev/roove/ui/common/custom/HorizontalCarouselLayoutManager.kt
+++ app/src/main/java/com/mmdev/roove/ui/common/custom/HorizontalCarouselLayoutManager.kt
@@ -1,7 +1,7 @@
 /*
  * Created by Andrii Kovalchuk
  * Copyright (c) 2020. All rights reserved.
- * Last modified 07.03.20 16:16
+ * Last modified 07.03.20 19:17
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
