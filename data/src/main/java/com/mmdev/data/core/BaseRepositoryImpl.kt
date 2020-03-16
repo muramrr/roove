41b9788e29af13bf292f34719c257d74d8fd8dcd
Andrii Kovalchuk <muram4ik@gmail.com>
2020-03-16 15:39:56 +0200

    fixed ajustviewbounds exception throwing on custom circle image view removed unused dimen

--- data/src/main/java/com/mmdev/data/core/BaseRepositoryImpl.kt
+++ data/src/main/java/com/mmdev/data/core/BaseRepositoryImpl.kt
@@ -1,7 +1,7 @@
 /*
  * Created by Andrii Kovalchuk
  * Copyright (c) 2020. All rights reserved.
- * Last modified 14.03.20 17:49
+ * Last modified 16.03.20 15:39
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -11,9 +11,7 @@
 package com.mmdev.data.core
 
 import com.google.firebase.firestore.DocumentReference
-import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.Query
 import com.mmdev.data.user.UserWrapper
 
 
@@ -22,40 +20,51 @@ import com.mmdev.data.user.UserWrapper
  */
 
 open class BaseRepositoryImpl constructor(private val firestore: FirebaseFirestore,
-                                          private val userWrapper: UserWrapper) {
+                                          private val userWrapper: UserWrapper): Reinitable {
 
-	internal var currentUser = userWrapper.getUser()
-	internal var currentUserId: String = currentUser.baseUserInfo.userId
+	protected var currentUser = userWrapper.getUser()
+	protected var currentUserId: String = currentUser.baseUserInfo.userId
 
-	internal var currentUserDocRef: DocumentReference = firestore.collection(USERS_COLLECTION_REFERENCE)
+	protected var currentUserDocRef: DocumentReference = firestore.collection(USERS_COLLECTION_REFERENCE)
 		.document(currentUser.baseUserInfo.city)
 		.collection(currentUser.baseUserInfo.gender)
 		.document(currentUser.baseUserInfo.userId)
 
-	internal var initialConversationsQuery: Query = currentUserDocRef
-		.collection(CONVERSATIONS_COLLECTION_REFERENCE)
-		.orderBy(CONVERSATION_TIMESTAMP_FIELD, Query.Direction.DESCENDING)
-		.whereEqualTo(CONVERSATION_STARTED_FIELD, true)
-		.limit(20)
 
+	override fun reInit() {
+		if (currentUser != userWrapper.getInMemoryUser()) {
+			currentUser = userWrapper.getUser()
+			currentUserId = currentUser.baseUserInfo.userId
 
-	internal lateinit var paginateLastConversationLoaded: DocumentSnapshot
-	internal lateinit var paginateConversationsQuery: Query
-
+			currentUserDocRef = firestore.collection(USERS_COLLECTION_REFERENCE)
+				.document(currentUser.baseUserInfo.city)
+				.collection(currentUser.baseUserInfo.gender)
+				.document(currentUser.baseUserInfo.userId)
+		}
+	}
 
 	internal val TAG = "mylogs_" + javaClass.simpleName
 
 	companion object {
-		// firestore users references
+		// firestore users collection references
 		const val USERS_COLLECTION_REFERENCE = "users"
+		const val USER_LIKED_COLLECTION_REFERENCE = "liked"
 		const val USER_MATCHED_COLLECTION_REFERENCE = "matched"
 		const val USER_SKIPPED_COLLECTION_REFERENCE = "skipped"
 		// firestore conversations reference
 		const val CONVERSATIONS_COLLECTION_REFERENCE = "conversations"
 		const val CONVERSATION_STARTED_FIELD = "conversationStarted"
 		const val CONVERSATION_TIMESTAMP_FIELD = "lastMessageTimestamp"
+		const val CONVERSATION_LAST_MESSAGE_TEXT_FIELD = "lastMessageText"
 		const val CONVERSATION_DELETED_FIELD = "conversationDeleted"
+		// Firebase Storage references
+		const val GENERAL_FOLDER_STORAGE_IMG = "images"
+		const val SECONDARY_FOLDER_STORAGE_IMG = "conversations"
+
+		const val MATCHED_DATE_FIELD = "matchedDate"
 
 		const val USER_ID_FIELD = "userId"
+
+
 	}
 }
--- data/src/main/java/com/mmdev/data/user/UserWrapper.kt
+++ data/src/main/java/com/mmdev/data/user/UserWrapper.kt
@@ -1,7 +1,7 @@
 /*
  * Created by Andrii Kovalchuk
  * Copyright (c) 2020. All rights reserved.
- * Last modified 14.03.20 17:01
+ * Last modified 16.03.20 14:38
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -11,21 +11,40 @@
 package com.mmdev.data.user
 
 import com.mmdev.business.core.UserItem
+import com.mmdev.business.local.LocalUserRepository
+import javax.inject.Inject
 import javax.inject.Singleton
 
 
 /**
  * wrapper current user to use as instance in singleton classes that requires actual user info
+ * after it changed (user edit some info)
  */
 
 @Singleton
-class UserWrapper {
+class UserWrapper @Inject constructor(private val localRepo: LocalUserRepository) {
 
-	private var user: UserItem = UserItem()
+	//get preCached user obj
+	private var user: UserItem = localRepo.getSavedUser()!!
 
-	fun getUser(): UserItem = user
+	//this fun is used to avoid allocate unused copy of this.user made by getUser() fun
+	//used to compare if user was changed
+	fun getInMemoryUser() = user
 
+	/**
+	 * Problem: data can be modified outside the scope of the setter method,
+	 * which breaks the encapsulation purpose of the setter
+	 * @param this.user is assigned to the method’s parameter variable directly.
+	 * That means both of the variables are referring to the same object in memory - userItem.
+	 * So changes made to either the @this.user or @userWrapper.getUserInMemory()
+	 * variables are actually made on the same object.
+	 * A workaround for this situation is to copy object from the private this.user var to the
+	 * new allocated object.
+	 * @return conditional constant related to this class
+	 */
+	fun getUser(): UserItem = user.clone()
 	fun setUser(user: UserItem) {
 		this.user = user
+		localRepo.saveUserInfo(user)
 	}
 }
